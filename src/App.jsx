559868208fe3b43a92c7b2fdd017d8575edfb4ac
王鹏斌 <wangpengbin@ctv.org.cn>
2020-01-09 17:13:33 +0800

    init project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Dot from './components/dot/index.jsx'
+
+const App = () => {
+  return (
+    <div>
+      <Dot />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/dot/index.jsx
@@ -0,0 +1,11 @@
+
+import React from 'react'
+import './index.scss'
+
+const Dot = () => {
+  return (
+    <div className='dot' />
+  )
+}
+
+export default Dot
