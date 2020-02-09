40b183d91921a79124369759adca5ad51a6c6729
王鹏斌 <wangpengbin@ctv.org.cn>
2020-02-09 17:21:29 +0800

    changes dot

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Dot from './components/dot';
+
+function App() {
+  return (
+    <div className="App">
+      <Dot />
+    </div>
+  );
+}
+
+export default App;
